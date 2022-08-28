e5a2405efed3767a2ac7da0ca789d2daab31ab6a
SergioGG27 <sagomezg55@gmail.com>
2022-08-28 07:51:49 -0500

    segundo comit

--- assets/code.js
+++ assets/code.js
@@ -1,18 +1,27 @@
 let etiquetaBody = document.querySelector('body')
 let navBar = document.createElement('nav')
-navBar.className = 'navbar navbar-expand-lg bg-light'
+navBar.className = 'navbar navbar-expand-lg bg-dark navbar-dark'
 etiquetaBody.appendChild(navBar)
 
 let divHijoNavbar = document.createElement('div')
-divHijoNavbar.className = 'container-fluid'
+divHijoNavbar.className = 'container'
 navBar.appendChild(divHijoNavbar)
 
-let divDivHijoNavbar = document.createElement('div')
-divDivHijoNavbar.className = 'nav-item'
-divHijoNavbar.appendChild(divDivHijoNavbar)
+let a_DivHijoNavbar = document.createElement('a')
+a_DivHijoNavbar.className = 'navbar-brand'
+a_DivHijoNavbar.setAttribute('href', '#')
+divHijoNavbar.appendChild(a_DivHijoNavbar)
 
-let a_divDivHijoNavbar = document.createElement('a')
-a_divDivHijoNavbar.className = 'nav-link'
-a_divDivHijoNavbar.setAttribute('href', '#')
-a_divDivHijoNavbar.innerText = 'INICIAR SECCION'
-divDivHijoNavbar.appendChild(a_divDivHijoNavbar)
+let imgHijoA = document.createElement('img')
+imgHijoA.setAttribute('src', 'assets/img/logo.svg')
+a_DivHijoNavbar.appendChild(imgHijoA)
+
+let botonHamburguesaResponsive = document.createElement('button')
+botonHamburguesaResponsive.className = 'navbar-toggler'
+botonHamburguesaResponsive.setAttribute('type', 'button')
+botonHamburguesaResponsive.setAttribute('data-bs-toggle', 'collapse')
+botonHamburguesaResponsive.setAttribute('data-bs-target', '#navbarSupportedContent')
+botonHamburguesaResponsive.setAttribute('aria-controls', 'navbarSupportedContent')
+botonHamburguesaResponsive.setAttribute('aria-expanded', 'false')
+botonHamburguesaResponsive.setAttribute('aria-label', 'Toggle navigation')
+divHijoNavbar.appendChild(botonHamburguesaResponsive)
